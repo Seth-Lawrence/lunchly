c28884b35f01e9d235d7350ff8cf591794b6571c
Teagan McClintock <teagan.mcclintock@gmail.com>
2024-01-03 03:06:10 -0500

    Added search functionality to get / route and modified search query in Customers

--- routes.js
+++ routes.js
@@ -13,7 +13,11 @@ const router = new express.Router();
 /** Homepage: show list of customers. */
 
 router.get("/", async function (req, res, next) {
-  const customers = await Customer.all();
+
+  const customers = req.query.search
+    ? await Customer.search(req.query.search)
+    : await Customer.all()
+
   return res.render("customer_list.html", { customers });
 });
 
